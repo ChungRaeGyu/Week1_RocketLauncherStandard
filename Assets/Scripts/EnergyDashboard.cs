c9257485372c58f47624660e6c047d99dad1be2a
RaeGyu <wogns9769@gmail.com>
2024-05-14 17:10:00 +0900

    람다식

--- Assets/Scripts/EnergyDashboard.cs
+++ Assets/Scripts/EnergyDashboard.cs
@@ -9,11 +9,18 @@ public class EnergyDashboard : MonoBehaviour
     private void Start()
     {
         // TODO : energySystem에서 값이 바뀌면 fillBar.fillAmount가 바뀌도록 변경
-        energySystem.OnEnergyChanged += EnergyChanged;
+        energySystem.OnEnergyChanged += obj =>fillBar.fillAmount -=obj;
     }
-
+    /*원래 식
+    private void Start(){
+        --핸들러 등록--
+        energySystem.OnEnergyChanged += EnergyChanged;
+        
+    }
     private void EnergyChanged(float obj)
     {
+        --핸들러를 통해 받아온 값을 가지고 행동하는 메소드--
         fillBar.fillAmount-= obj;
     }
+    */
 }
